d046b6c232eacfbfe3bc769a868a6bbefa38025b
Yohanan Simão <yohanansimao7@gmail.com>
2024-04-30 08:20:00 -0300

    feat: Correção do CSS, implementação de mais ações no JS.
    Correção do CSS que estava utilizando position absolute em quase tudo, adição de ações nas partes adjacentes do catálogo, e começo do desenvolvimento do footer.

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 window.onscroll = function () {
   
-  var sobe = document.getElementById('up')
-  var catalog = document.getElementById('catalog-show')
-  var catalogicon = document.getElementById('catalog-icon')
-
-  if (window.scrollY > 200 && sobe){
+  var sobe = document.querySelector('#up')
+  var catalog = document.querySelector('#catalog-show')
+  var catalog2 = document.querySelector('#catalog-show2')
+  var catalog3 = document.querySelector('#catalog-show3')
+  var catalog4 = document.querySelector('#catalog-show4')
+  var catalog5 = document.querySelector('#catalog-show5')
+  var catalogicon = document.querySelector('#catalog-icon')
+  var catalogL = document.querySelector('#catalog-show')
+  
+  if (window.scrollY > 1200){
     sobe.classList.add('activated-up')
-    console.log('icone foi')
+    sobe.classList.remove('.catalog-icon')
   } else {
     sobe.classList.remove('activated-up')
+    console.log('deu erro')
   }
   
-  if (window.scrollY > 80 && catalog) {
+  if (window.scrollY > 50 && catalog) {
     catalog.classList.add('catalog-active-up')
-    console.log('catalago foi')
+    catalog2.classList.add('catalog-active-up')
+    catalog3.classList.add('catalog-active-up')
+    catalog4.classList.add('catalog-active-up')
+    catalog5.classList.add('catalog-active-up')
   } else {
     catalog.classList.remove('catalog-active-up')
+    catalog2.classList.remove('catalog-active-up')
+    catalog3.classList.remove('catalog-active-up')
+    catalog4.classList.remove('catalog-active-up')
+    catalog5.classList.remove('catalog-active-up')
   }
 
-  if (window.scrollY > 70 && catalogicon) {
+  if (window.scrollY > 50 && catalogicon) {
     catalogicon.classList.add('catalog-icon')
-    console.log('ícone do catalogo foi')
   } else {
     catalogicon.classList.remove('catalog-icon')
+  }
+
+  if (window.scroll = 0 && catalogL) {
+    catalogL.classList.add('invisible')
+  } else {
+    console.log('deu pau')
   }
 };
